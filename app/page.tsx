c6bbb8c27e7ceba9981adc93d3b0330e30f047da
elsnoty <mostafashawky399@gmail.com>
2025-03-22 08:06:58 +0200

    hot fix#4

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import { createClient } from "@/utils/supabase/server";
 import { signout } from "./(auth)/signout/page";
-import DeleteUserButton from "./home/page";
 
 export default async function Home() {
 
@@ -16,7 +15,6 @@ export default async function Home() {
         <button formAction={signout} className="bg-red-500 text-white px-4 py-2 rounded">
           Log Out
         </button>
-        <DeleteUserButton userId="94b115f6-5c9e-4de8-b6fb-977b9ae289e1" />
       </form>
     </div>
   );
